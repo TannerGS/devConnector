c73d8be77ca14a7e80c66dc91a52c01991559a60
Eugene-Krabs <tannersimpkins@gmail.com>
2019-06-24 13:39:30 -0500

    Added user registration logic and password encryption

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const db = config.get('MongoURI');
 const ConnectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useCreateIndex: true
     });
 
     console.log('MongoDB connected...');
